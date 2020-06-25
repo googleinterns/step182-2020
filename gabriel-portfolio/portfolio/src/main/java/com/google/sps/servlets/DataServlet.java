9a7a0a4bcb0b6a96f5f9e7d40e751c7cf747006d
Gabriel <gabrielgonzalezdiaz2001@gmail.com>
2020-06-25 17:12:57 +0000

    fix indentation, spacing, and add photo album

--- gabriel-portfolio/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ gabriel-portfolio/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -21,12 +21,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
-}
+// Did not remove this because it will be used in week 3
+// @WebServlet("/data")
+// public class DataServlet extends HttpServlet {
+//   @Override
+//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//     response.setContentType("text/html;");
+//     response.getWriter().println("<h1>Hello world!</h1>");
+//   }
+// }
