f00ccef3024d7d45292630a3249a62da26796759
Pierce <pierce.d.wilson.23@dartmouth.edu>
2020-07-15 05:20:13 +0000

    added putting comments to datastore

--- pierce-portfolio/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ pierce-portfolio/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,17 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,18 +42,43 @@ import java.util.Calendar;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   private static final Gson gson = new Gson();
-  private List <Comment> theComments = new ArrayList<Comment>();  
   
+  final String commentEntity = "Comment";
+  final String nameProperty = "name";
+  final String textProperty = "text";
+  final String timestampProperty = "timestamp";
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    List <Comment> theComments = new ArrayList<Comment>();  
+
+    for (Entity entity : results.asIterable()) {
+        theComments.add(new Comment(entity.getProperty("name").toString(),entity.getProperty("text").toString(),entity.getProperty("timestamp").toString()));
+    }
     response.setContentType("application/json;");
     String jsonText =  gson.toJson(theComments);
+
     response.getWriter().println(jsonText);
   }
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    theComments.add(new Comment(request.getParameter("name-input"), request.getParameter("comment-input")));
+    
+    Comment tComment = new Comment(request.getParameter("name-input"), request.getParameter("comment-input"));
+
+    Entity myComment = new Entity(commentEntity);
+    myComment.setProperty(nameProperty, tComment.getName());
+    myComment.setProperty(textProperty, tComment.getText());
+    myComment.setProperty(timestampProperty, tComment.getTimestamp());
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(myComment);
+
+    datastore.put(myComment);
+
     response.sendRedirect("/comments.html");
   }
 }
