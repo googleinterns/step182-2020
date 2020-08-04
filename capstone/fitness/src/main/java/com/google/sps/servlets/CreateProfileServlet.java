6b63da6613796cb837217d1be1a1cc278faaeb0b
Gabriel <gabrielgonzalezdiaz2001@gmail.com>
2020-08-04 19:04:48 +0000

    refactor

--- capstone/fitness/src/main/java/com/google/sps/servlets/CreateProfileServlet.java
+++ capstone/fitness/src/main/java/com/google/sps/servlets/CreateProfileServlet.java
@@ -10,7 +10,7 @@ import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.sps.util.UserHelper;
+import com.google.sps.util.DataHandler;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -31,8 +31,8 @@ public class CreateProfileServlet extends HttpServlet {
 
     // Set up datastore.
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Key userKey = KeyFactory.createKey(UserHelper.USER_ENTITY, userEmail);
-    Query query = new Query(UserHelper.USER_ENTITY).addFilter(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, userKey).setKeysOnly();
+    Key userKey = KeyFactory.createKey(DataHandler.USER_ENTITY, userEmail);
+    Query query = new Query(DataHandler.USER_ENTITY).addFilter(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, userKey).setKeysOnly();
     PreparedQuery results = datastore.prepare(query);
 
     // If the user is already in datastore, we do not need to re-create profile.
@@ -52,21 +52,21 @@ public class CreateProfileServlet extends HttpServlet {
     String userEmail = userService.getCurrentUser().getEmail();
 
     // Get the parameters from the request.
-    String name = request.getParameter(UserHelper.NAME_PROPERTY);
-    Integer age = Integer.parseInt(request.getParameter(UserHelper.AGE_PROPERTY));
-    Integer weeksTotrain = Integer.parseInt(request.getParameter(UserHelper.WEEKS_TO_TRAIN_PROPERTY));
-    Float lengthOfMarathon = Float.parseFloat(request.getParameter(UserHelper.MARATHON_LENGTH_PROPERTY));
-    Float initialTime = Float.parseFloat(request.getParameter(UserHelper.INITIAL_TIME_PROPERTY));
-    Float goalTime = Float.parseFloat(request.getParameter(UserHelper.GOAL_TIME_PROPERTY));
+    String name = request.getParameter(DataHandler.NAME_PROPERTY);
+    Integer age = Integer.parseInt(request.getParameter(DataHandler.AGE_PROPERTY));
+    Integer weeksTotrain = Integer.parseInt(request.getParameter(DataHandler.WEEKS_TO_TRAIN_PROPERTY));
+    Float lengthOfMarathon = Float.parseFloat(request.getParameter(DataHandler.MARATHON_LENGTH_PROPERTY));
+    Float initialTime = Float.parseFloat(request.getParameter(DataHandler.INITIAL_TIME_PROPERTY));
+    Float goalTime = Float.parseFloat(request.getParameter(DataHandler.GOAL_TIME_PROPERTY));
 
     // Create a user entity that uses the email as the key.
-    Entity newUser = new Entity(UserHelper.USER_ENTITY, userEmail);
-    newUser.setProperty(UserHelper.NAME_PROPERTY, name);
-    newUser.setProperty(UserHelper.MARATHON_LENGTH_PROPERTY, lengthOfMarathon);
-    newUser.setProperty(UserHelper.WEEKS_TO_TRAIN_PROPERTY, weeksTotrain);
-    newUser.setProperty(UserHelper.PROGRESS_PROPERTY, "[]");
-    newUser.setProperty(UserHelper.INITIAL_TIME_PROPERTY, initialTime);
-    newUser.setProperty(UserHelper.GOAL_TIME_PROPERTY, goalTime);
+    Entity newUser = new Entity(DataHandler.USER_ENTITY, userEmail);
+    newUser.setProperty(DataHandler.NAME_PROPERTY, name);
+    newUser.setProperty(DataHandler.MARATHON_LENGTH_PROPERTY, lengthOfMarathon);
+    newUser.setProperty(DataHandler.WEEKS_TO_TRAIN_PROPERTY, weeksTotrain);
+    newUser.setProperty(DataHandler.PROGRESS_PROPERTY, "[]");
+    newUser.setProperty(DataHandler.INITIAL_TIME_PROPERTY, initialTime);
+    newUser.setProperty(DataHandler.GOAL_TIME_PROPERTY, goalTime);
 
     // Put user in datastore.
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
