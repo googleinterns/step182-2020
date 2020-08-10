abdd2214541eaae6bbfb0126bd037abadece3b78
Pierce <pierce.d.wilson.23@dartmouth.edu>
2020-08-10 20:04:08 +0000

    Basic GetFreeTime function

---
+++ capstone/fitness/src/main/java/com/google/sps/util/Scheduler.java
@@ -0,0 +1,73 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. 
+package com.google.sps.util;
+
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.Event;
+import java.util.*;
+
+// class scheduler uses the user's free/busy information and the duration of an exercise to find a time in a user's day when
+// the user could do an exercise
+public class Scheduler {
+//   private final ScheduleAdapter scheduleAdapter;
+//   private final CalendarAdapter calendarAdapter;
+  private final long exerciseDuration;
+
+
+  public Scheduler(/*ScheduleAdapter scheduleAdapter, CalendarAdapter calendarAdapter,*/ long exerciseDuration) {
+    // this.scheduleAdapter = scheduleAdapter;
+    // this.calendarAdapter = calendarAdapter;
+    this.exerciseDuration =exerciseDuration;
+  }
+ 
+  // minSpan and maxSpan are the boundaries for when a workout could be scheduled on a particular day e.g 7:00 AM to 9:00 PM
+  // events is the user's free/busy information.
+  public Event GetFreeTime(/*EventDateTime day,*/ EventDateTime minSpan, EventDateTime maxSpan, Collection<Event> events) {
+    
+    // initial capacity of eventsQueue is 1 larger than the size of events so that it can handle the case
+    // where there are no events. 
+    PriorityQueue<Event> eventQueue = new PriorityQueue<Event>((events.size() +1), new EventComparator());
+    for (Event evt : events){
+        eventQueue.add(evt);
+    }
+    
+    EventDateTime now = minSpan;
+    EventDateTime end = maxSpan;
+
+
+    long exerciseMilliseconds = this.exerciseDuration * Time.minutesToMilliseconds;
+    
+    // if there is no space for an exercise between 'now' and the next event, then dequeue the first event.
+    while (eventQueue.size() > 0 && Time.eventDateTimeToMilliseconds(eventQueue.peek().getStart()) - Time.eventDateTimeToMilliseconds(now) < (exerciseMilliseconds)) {
+      now = eventQueue.poll().getEnd();
+      }
+    // once broken out of the loop (i.e the user has no more events),
+    // check that there is enough time, and schedule at now. 
+    if (Time.eventDateTimeToMilliseconds(now) > Time.eventDateTimeToMilliseconds(end) - (exerciseMilliseconds)) {
+      return null;
+     }  
+    
+    Event event = new Event();
+    event.setStart(now);
+    long endValue = Time.eventDateTimeToMilliseconds(now) + exerciseMilliseconds;
+    DateTime endDT = new DateTime(endValue);
+    EventDateTime e = new EventDateTime();
+    e.setDateTime(endDT);
+    event.setEnd(e);
+    event.setDescription("success"); 
+    return event;  
+
+  }
+}
---
+++ capstone/fitness/src/test/java/com/google/sps/SchedulerTest.java
@@ -0,0 +1,147 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps;
+import com.google.sps.util.*;
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.Event;
+import static org.hamcrest.Matchers.*;
+
+public class SchedulerTest { 
+  long duration = 30;
+  Scheduler scheduler= new Scheduler(duration);
+
+  
+  // tests a scenario where there is space after all of the events and before maxSpan. 
+  @Test
+  public void SpaceAfterEventsInWindow() {
+    Collection<Event> events = new ArrayList<Event>();
+    // events.add(new EasyEvent(15, 50));
+    // events.add(new EasyEvent(70,100));
+
+    Event b = new Event();
+    // DateTime d = new DateTime("2020-08-10T10:00:00-04:00");
+    b.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:00:00-04:00")));
+    b.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:30:00-04:00")));
+
+    Event e = new Event();
+    e.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:45:00-04:00")));
+    e.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T13:30:00-04:00")));
+   
+    events.add(b);
+    events.add(e);
+    
+    EventDateTime minSpan = new EventDateTime();
+    minSpan.setDateTime(new DateTime("2020-08-10T10:00:00-04:00"));
+
+    EventDateTime maxSpan = new EventDateTime();
+    maxSpan.setDateTime(new DateTime("2020-08-11T00:00:00-04:00"));
+
+    Event exerciseTime = this.scheduler.GetFreeTime(minSpan,maxSpan,events);
+    
+    //tests that exercise starts at the end of the last event.
+    assertEquals(exerciseTime.getStart() ,e.getEnd());
+    
+    //tests that exercise ends 30 minutes after it starts. 
+    assertEquals(Time.getMillisecondsBetween(exerciseTime.getStart(),exerciseTime.getEnd()), duration * Time.minutesToMilliseconds);
+
+    // tests that exercise is not after and maxSpan.
+    assertThat(Time.getMillisecondsBetween(exerciseTime.getEnd(),maxSpan), greaterThan(0L));
+    
+    // tests that exercise is not before and minSpan.
+    // assertGreaterThan(Time.eventDateTimeToMilliseconds(exerciseTime.getStart()), Time.eventDateTimeToMilliseconds(minSpan));
+    assertThat(Time.getMillisecondsBetween(minSpan, exerciseTime.getStart()), greaterThan(0L));
+  }
+
+//   tests a scenario where there is space for an exercise between two events. 
+  @Test
+  public void SpaceBetweenTwoEvents() {
+    Collection<Event> events = new ArrayList<Event>();
+
+
+    Event a = new Event();
+    a.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:00:00-04:00")));
+    a.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:30:00-04:00")));
+
+    Event v = new Event();
+    v.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T12:45:00-04:00")));
+    v.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T13:30:00-04:00")));
+   
+    events.add(a);
+    events.add(v);
+    
+    EventDateTime minSpan = new EventDateTime();
+    minSpan.setDateTime(new DateTime("2020-08-10T10:00:00-04:00"));
+
+    EventDateTime maxSpan = new EventDateTime();
+    maxSpan.setDateTime(new DateTime("2020-08-11T00:00:00-04:00"));
+
+    Event exerciseTime = this.scheduler.GetFreeTime(minSpan,maxSpan,events);
+    
+    //tests that exercise starts at the end of the first event.
+    assertTrue(exerciseTime.getStart().getDateTime().getValue() == a.getEnd().getDateTime().getValue());
+    
+    //tests that exercise ends 30 minutes after it starts. 
+    assertTrue(exerciseTime.getEnd().getDateTime().getValue() == exerciseTime.getStart().getDateTime().getValue()+ (duration * 60000));
+
+    // tests that exercise is not after and maxSpan.
+    assertTrue(exerciseTime.getEnd().getDateTime().getValue() < maxSpan.getDateTime().getValue());
+    
+    // tests that exercise is not before and minSpan.
+    assertTrue(exerciseTime.getStart().getDateTime().getValue() > minSpan.getDateTime().getValue());
+
+  }
+
+  // tests a scenario where the user has no events whatsoever.
+  @Test
+  public void NoEvents() {
+    Collection<Event> events = new ArrayList<Event>();
+    
+    EventDateTime minSpan = new EventDateTime();
+    minSpan.setDateTime(new DateTime("2020-08-10T10:00:00-04:00"));
+
+    EventDateTime maxSpan = new EventDateTime();
+    maxSpan.setDateTime(new DateTime("2020-08-11T00:00:00-04:00"));
+
+    Event exerciseTime = this.scheduler.GetFreeTime(minSpan,maxSpan,events);
+    // tests that the exercise begins at minSpan since the user has no events.
+    assertTrue(exerciseTime.getStart().getDateTime().getValue() == minSpan.getDateTime().getValue());
+  }
+
+  // tests scenario where there's no space for events.  
+  @Test
+  public void NoSpace() {
+    Collection<Event> events = new ArrayList<Event>();
+    
+    EventDateTime minSpan = new EventDateTime();
+    minSpan.setDateTime(new DateTime("2020-08-10T10:00:00-04:00"));
+
+    EventDateTime maxSpan = new EventDateTime();
+    maxSpan.setDateTime(new DateTime("2020-08-11T00:00:00-04:00"));
+    
+
+    Event h = new Event();
+    h.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:10:00-04:00")));
+    h.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T23:55:00-04:00")));
+    events.add(h);
+
+    Event exerciseTime = this.scheduler.GetFreeTime(minSpan,maxSpan,events);
+    assertNull(exerciseTime);
+  }
+}
+
