391aec6508ebfe7c4dc3e15d72081bd9f76d4910
Ikenna Elue <jonathanelue@yahoo.com>
2020-08-12 19:48:13 +0000

    Start Json Prep

--- capstone/fitness/src/main/java/com/google/sps/progress/GoalStep.java
+++ capstone/fitness/src/main/java/com/google/sps/progress/GoalStep.java
@@ -20,12 +20,21 @@ import java.util.*;
 
 /* Progress marker that holds an Exercise. */
 public class GoalStep extends BananaNode {
+  private static final long serialVersionUID = -2478810042129355868L;     
 
   private final Exercise marker;
 
   public GoalStep(Exercise marker) {
     super();
     this.marker = marker;
+  }
+
+  public GoalStep(JsonGoalStep jgs) {
+    super();
+    this.marker = new Exercise.Builder(jgs.getName())
+                      .addSetValues(jgs.getSetValues())
+                      .build();
+    setComplete(jgs.isComplete());
   }
 
   public Exercise getMarker() {
