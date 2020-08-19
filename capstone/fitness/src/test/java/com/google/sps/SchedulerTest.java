66cd32cc1454b0c00c69947227cfcbe629f9c402
Pierce <pierce.d.wilson.23@dartmouth.edu>
2020-08-19 15:26:10 +0000

    added tests for overlapping events.

--- capstone/fitness/src/test/java/com/google/sps/SchedulerTest.java
+++ capstone/fitness/src/test/java/com/google/sps/SchedulerTest.java
@@ -157,4 +157,105 @@ public class SchedulerTest {
 
     assertTrue(exerciseTime.getStart().getDateTime().getValue()==t.getEnd().getDateTime().getValue());
      }
+  
+  // Test for overlapping events where one starts and ends within the duration of the other.
+  @Test 
+  public void overlapOne(){
+    Collection<Event> events = new ArrayList<Event>();
+    
+    DateTime minSpan = new DateTime("2020-08-10T10:00:00-04:00");
+    DateTime maxSpan = new DateTime("2020-08-11T00:00:00-04:00");
+    
+    Event b = new Event();
+    b.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:00:00-04:00")));
+    b.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T12:30:00-04:00")));
+ 
+    Event e = new Event();
+    e.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:45:00-04:00")));
+    e.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T11:30:00-04:00")));
+   
+    Event p = new Event();
+    p.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T14:45:00-04:00")));
+    p.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T15:00:00-04:00")));
+    
+    events.add(b);
+    events.add(e);
+    events.add(p);
+
+
+    Event exerciseTime = this.scheduler.getFreeTime(minSpan, maxSpan, events);
+    
+    assertTrue(exerciseTime.getStart().getDateTime().getValue() == b.getEnd().getDateTime().getValue());
+    
+    //Tests that exercise ends 30 minutes after it starts. 
+    assertTrue(exerciseTime.getEnd().getDateTime().getValue() == exerciseTime.getStart().getDateTime().getValue()+ (duration * 60000));
+ 
+    
+    // Tests that exercise is not after and maxSpan.
+    assertTrue(exerciseTime.getEnd().getDateTime().getValue() < maxSpan.getValue());
+
+    
+    // Tests that exercise is not before and minSpan.
+    assertTrue(exerciseTime.getStart().getDateTime().getValue() > minSpan.getValue());
+
+     }
+
+  // Test for overlapping events where one starts during another and ends after the first one ends.
+  @Test 
+  public void overlapTwo(){
+    Collection<Event> events = new ArrayList<Event>();
+    
+    DateTime minSpan = new DateTime("2020-08-10T10:00:00-04:00");
+    DateTime maxSpan = new DateTime("2020-08-11T00:00:00-04:00");
+    
+    
+
+    Event j = new Event();
+    j.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:10:00-04:00"))); 
+    j.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:45:30-04:00")));
+    events.add(j);
+
+    Event t = new Event();
+    t.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:30:00-04:00"))); 
+    t.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T11:00:30-04:00")));
+    events.add(t);
+
+
+    Event exerciseTime = this.scheduler.getFreeTime(minSpan, maxSpan, events);
+    
+    assertTrue(exerciseTime.getStart().getDateTime().getValue() == t.getEnd().getDateTime().getValue());
+    
+    //Tests that exercise ends 30 minutes after it starts. 
+    assertTrue(exerciseTime.getEnd().getDateTime().getValue() == exerciseTime.getStart().getDateTime().getValue()+ (duration * 60000));
+ 
+    
+    // Tests that exercise is not after and maxSpan.
+    assertTrue(exerciseTime.getEnd().getDateTime().getValue() < maxSpan.getValue());
+
+    
+    // Tests that exercise is not before and minSpan.
+    assertTrue(exerciseTime.getStart().getDateTime().getValue() > minSpan.getValue());
+     }
+  
+  // Test for overlapping events where two events start at the same time but have different end times.
+  @Test 
+  public void overlapThree(){
+    Collection<Event> events = new ArrayList<Event>();
+    
+    DateTime minSpan = new DateTime("2020-08-10T10:00:00-04:00");
+    DateTime maxSpan = new DateTime("2020-08-11T00:00:00-04:00");
+    
+    Event t = new Event();
+    t.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:10:00-04:00"))); 
+    t.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:45:30-04:00")));
+    events.add(t);
+    
+    Event g = new Event();
+    g.setStart(new EventDateTime().setDateTime(new DateTime("2020-08-10T10:10:00-04:00"))); 
+    g.setEnd(new EventDateTime().setDateTime(new DateTime("2020-08-10T11:45:30-04:00")));
+    events.add(g);
+    Event exerciseTime = this.scheduler.getFreeTime(minSpan, maxSpan, events);
+
+    assertTrue(exerciseTime.getStart().getDateTime().getValue()==g.getEnd().getDateTime().getValue());
+     }
 }
