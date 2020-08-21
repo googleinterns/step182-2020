8334d5220998ab2e1ae1385e7a081b852799e733
Pierce <pierce.d.wilson.23@dartmouth.edu>
2020-08-21 13:42:45 +0000

    fixed styling in Utils.java

--- capstone/fitness/src/main/java/com/google/sps/util/Utils.java
+++ capstone/fitness/src/main/java/com/google/sps/util/Utils.java
@@ -18,18 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.ServletException;
 import java.util.*;
 
-
-
-
-
-
 class Utils {
   /**
-   * Global instance of the {@link DataStoreFactory}. The best practice is to make it a single
-   * globally shared instance across your application.
+   * Global instance of the {@link DataStoreFactory}. 
    */
-  private static final AppEngineDataStoreFactory DATA_STORE_FACTORY =
-      AppEngineDataStoreFactory.getDefaultInstance();
+  private static final AppEngineDataStoreFactory DATA_STORE_FACTORY = AppEngineDataStoreFactory.getDefaultInstance();
   
   /** Global instance of the HTTP transport. */
   static final HttpTransport HTTP_TRANSPORT = new UrlFetchTransport();
@@ -40,12 +33,9 @@ class Utils {
 
   static GoogleClientSecrets getClientCredential() throws IOException {
     if (clientSecrets == null) {
-      clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-          new InputStreamReader(Utils.class.getResourceAsStream("/client_secret.json")));
-      Preconditions.checkArgument(!clientSecrets.getDetails().getClientId().startsWith("Enter ")
-          && !clientSecrets.getDetails().getClientSecret().startsWith("Enter "),
-          "Download client_secrets.json file from https://code.google.com/apis/console/"
-          + "?api=calendar into calendar-appengine-sample/src/main/resources/client_secrets.json");
+      clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(Utils.class.getResourceAsStream("/client_secret.json")));
+      Preconditions.checkArgument(!clientSecrets.getDetails().getClientId().startsWith("Enter ") && !clientSecrets.getDetails().getClientSecret().startsWith("Enter "),
+        "Download client_secrets.json file from https://code.google.com/apis/console/"+ "?api=calendar into calendar-appengine-sample/src/main/resources/client_secrets.json");
     }
     return clientSecrets;
   }
@@ -55,8 +45,7 @@ class Utils {
   }
 
   static GoogleAuthorizationCodeFlow newFlow() throws IOException {
-    return new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-        getClientCredential(), Collections.singleton(CalendarScopes.CALENDAR)).setDataStoreFactory(
-        DATA_STORE_FACTORY).setAccessType("offline").build();
+    return new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, getClientCredential(), 
+      Collections.singleton(CalendarScopes.CALENDAR)).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
   }
 }
