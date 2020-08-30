906edd03960a2b870f2202285cbca8f441e6c518
Pierce <pierce.d.wilson.23@dartmouth.edu>
2020-08-30 16:01:59 +0000

    turns out i actually needed one of the imports i deleted haha

--- capstone/fitness/src/main/java/com/google/sps/servlets/CalendarDisplayServlet.java
+++ capstone/fitness/src/main/java/com/google/sps/servlets/CalendarDisplayServlet.java
@@ -8,6 +8,7 @@ import com.google.sps.util.*;
 import java.io.IOException;
 import java.util.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
